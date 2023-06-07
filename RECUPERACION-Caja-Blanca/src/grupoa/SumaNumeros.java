1db592f0a63b418d38ee1f0e50f1d741787b7e52
enieves <enieves@106-02.info2>
2023-06-07 12:24:11 +0200

    test

--- RECUPERACION-Caja-Blanca/src/grupoa/SumaNumeros.java
+++ RECUPERACION-Caja-Blanca/src/grupoa/SumaNumeros.java
@@ -4,7 +4,8 @@ public class SumaNumeros {
 
 	public int sumaNumeros(int num) {
 		int suma = 0;
-		for (int i = 1; i < num; i++) {
+		//se cambia i<num por i<=num
+		for (int i = 1; i <= num; i++) {
 			if(i%2==0) {
 				suma += i;
 			}
